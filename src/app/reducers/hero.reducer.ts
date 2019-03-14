1fc29600518e174c9cea9608527d4d6dc90bd307
Devlin Duldulao <webmasterdevlin@gmail.com>
2019-03-14 16:05:17 +0800

    added state fetching for future spinner component

--- src/app/reducers/hero.reducer.ts
+++ src/app/reducers/hero.reducer.ts
@@ -5,12 +5,14 @@ import { Hero } from "../models/hero.model";
 export interface HeroState {
   heroes: Hero[];
   hero: Hero;
+  fetching: boolean;
   error: string;
 }
 
 export const initialState: HeroState = {
   heroes: [],
   hero: {} as Hero,
+  fetching: false,
   error: ""
 };
 
@@ -19,43 +21,57 @@ export function heroReducer(
   action: HeroActions
 ): HeroState {
   switch (action.type) {
+    case HeroActionTypes.LoadHeroes:
+      return { ...state, fetching: true };
     case HeroActionTypes.LoadHeroesSuccess:
-      return { ...state, heroes: action.payload };
+      return { ...state, heroes: action.payload, fetching: false };
     case HeroActionTypes.LoadHeroesFail:
-      return { ...state, heroes: [], error: action.payload };
+      return { ...state, heroes: [], error: action.payload, fetching: false };
+    case HeroActionTypes.CreateHero:
+      return { ...state, fetching: true };
     case HeroActionTypes.CreateHeroSuccess:
       return {
         ...state,
-        heroes: [...state.heroes, action.payload]
+        heroes: [...state.heroes, action.payload],
+        fetching: false
       };
     case HeroActionTypes.CreateHeroFail:
       return {
         ...state,
-        error: action.payload
+        error: action.payload,
+        fetching: false
       };
+    case HeroActionTypes.UpdateHero:
+      return { ...state, fetching: true };
     // Not applicable on this app because this is a separate page
     case HeroActionTypes.UpdateHeroSuccess:
       return {
         ...state,
         heroes: state.heroes.map(hero =>
           hero.id === action.payload.id ? action.payload : hero
-        )
+        ),
+        fetching: false
       };
     // Not applicable on this app because this is a separate page
     case HeroActionTypes.UpdateHeroFail:
       return {
         ...state,
-        error: action.payload
+        error: action.payload,
+        fetching: false
       };
+    case HeroActionTypes.DeleteHero:
+      return { ...state, fetching: true };
     case HeroActionTypes.DeleteHeroSuccess:
       return {
         ...state,
-        heroes: state.heroes.filter(hero => hero.id !== action.payload)
+        heroes: state.heroes.filter(hero => hero.id !== action.payload),
+        fetching: false
       };
     case HeroActionTypes.DeleteHeroFail:
       return {
         ...state,
-        error: action.payload
+        error: action.payload,
+        fetching: false
       };
     default:
       return state;
